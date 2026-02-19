486d0cb49db2dddb5f810a02519f5c93842b1a7f
Marco <marcobrunno454@gmail.com>
2026-02-19 21:17:35 +0000

    Remoção do ficheiro teste.py

--- teste_bcrypt.py
+++
@@ -1,9 +0,0 @@
-# from passlib.context import CryptContext
-#
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-#
-# password = "abc123"
-#
-# hashed = pwd_context.hash(password)
-#
-# print("HASH GERADO:", hashed)
